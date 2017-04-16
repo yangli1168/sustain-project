0393693e914d7638d4d48a2e21a860764e889327
yangli1168 <350895216@qq.com>
2017-04-16 21:03:14 +0800

    a

--- pro-demo/src/main/java/net/xinqushi/util/ElasticSearchUtil.java
+++ pro-demo/src/main/java/net/xinqushi/util/ElasticSearchUtil.java
@@ -3,15 +3,24 @@ package net.xinqushi.util;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.lucene.search.TermQuery;
+import org.elasticsearch.action.index.IndexRequestBuilder;
 import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.search.SearchRequestBuilder;
+import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.Client;
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.settings.ImmutableSettings;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.elasticsearch.common.xcontent.XContentBuilder;
+import org.elasticsearch.index.query.QueryBuilder;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.SearchHits;
 
 import com.alibaba.fastjson.JSON;
 
+import net.sf.json.util.JSONBuilder;
 import net.xinqushi.orm.entity.City;
 
 /**
@@ -21,14 +30,17 @@ import net.xinqushi.orm.entity.City;
  * @Description ElasticSearch工具类
  */
 public class ElasticSearchUtil {
-	
-	//初始化
+
+	// 初始化
 	private Client client = null;
-	
+
 	/**
 	 * 私有构造方法
-	 * @param clusterName 不传值时默认"Yang"
-	 * @param host 不传值时默认"127.0.0.1"
+	 * 
+	 * @param clusterName
+	 *            不传值时默认"Yang"
+	 * @param host
+	 *            不传值时默认"127.0.0.1"
 	 * @return
 	 */
 	public ElasticSearchUtil(String clusterName, String host) {
@@ -36,46 +48,85 @@ public class ElasticSearchUtil {
 			client = this.getESClient(clusterName, host);
 		}
 	}
-	
+
 	@SuppressWarnings("resource")
-	private Client getESClient(String clusterName, String host){
+	private Client getESClient(String clusterName, String host) {
 		if (null == client) {
-			//配置client
+			// 配置client
 			Settings settings = ImmutableSettings.settingsBuilder()
-					.put("cluster.name", clusterName==null?"Yang":clusterName).build();
+					.put("cluster.name", clusterName == null ? "Yang" : clusterName).build();
 			client = new TransportClient(settings)
-					.addTransportAddress(new InetSocketTransportAddress(host==null?"127.0.0.1":host, 9300));
+					.addTransportAddress(new InetSocketTransportAddress(host == null ? "127.0.0.1" : host, 9300));
 		}
 		return client;
 	}
-	
-	public void putDoc(List<City> cities){
-		
-		for(int i=0; i<cities.size(); i++){
+
+	public void putDoc(List<City> cities) {
+
+		for (int i = 0; i < cities.size(); i++) {
 			Object just = cities.get(i);
-			IndexResponse response = client.prepareIndex().setIndex("city").setType("class").setId(i + 1 + "").setSource(JSON.toJSONString(just))
-					.execute().actionGet();
+			IndexResponse response = client.prepareIndex().setIndex("city").setType("class").setId(i + 1 + "")
+					.setSource(JSON.toJSONString(just)).execute().actionGet();
 			System.out.println("索引创建成功,版本号：" + response.getVersion());
 		}
 		client.close();
 	}
-	
+
 	public static void main(String[] args) {
-		List<City> cities = new ArrayList<City>();
-		
+		// List<City> cities = new ArrayList<City>();
+		//
+		// City city = new City();
+		// city.setId(888);
+		// city.setName("青云");
+		// city.setCreateTime(System.currentTimeMillis());
+		// cities.add(city);
+		//
+		// String jsondata = JSON.toJSONString(cities);
+		// System.out.println("封装的json:" + jsondata);
+		//
+		ElasticSearchUtil esUtil = new ElasticSearchUtil("Yang", "localhost");
+		// esUtil.putDoc(cities);
+//		System.out.println(esUtil.test());
+		esUtil.getInfo();
+	}
+
+	public String test() {
 		City city = new City();
 		city.setId(888);
 		city.setName("青云");
 		city.setCreateTime(System.currentTimeMillis());
-		cities.add(city);
-		
-		String jsondata = JSON.toJSONString(cities);
-		System.out.println("封装的json:" + jsondata);
-		
-		ElasticSearchUtil esUtil = new ElasticSearchUtil("Yang", "localhost");
-		esUtil.putDoc(cities);
-		
+		String sourceStr = JSON.toJSONString(city);
+		return indexInfo("a","b",sourceStr,30000);
 	}
 	
+	/**
+	 * 
+	 * @param indexName 索引名
+	 * @param typeName	类型名
+	 * @param sourceStr 需要索引的内容【json格式】
+	 * @param time 有效时间
+	 * @return id
+	 */
+	public String indexInfo(String indexName, String typeName, String sourceStr, long time) {
+		IndexRequestBuilder builder = client.prepareIndex().setIndex(indexName).setType(typeName)
+				.setSource(sourceStr.getBytes()).setTTL(time);
+//		builder.setId("special");
+		IndexResponse response = builder.execute().actionGet();
+		String id = response.getId();
+		return id;
+	}
 	
+	public String getInfo(){
+		String query = "{\"name\":\"测\"}";
+		SearchResponse response = client.prepareSearch("city")
+				.setQuery("name").setFrom(0).setSize(5).setExplain(true)
+				.execute().actionGet();
+		SearchHits hits = response.getHits();
+		for (SearchHit searchHit : hits) {
+			System.out.println(searchHit.getId()
+					+ " -> " + searchHit.getIndex()
+					+ " -> " + searchHit.getFields());
+		}
+		return null;
+	}
 }
