aea2776fd2353bbb2bd143f1b03e6d710cdb3952
yangli1168 <350895216@qq.com>
2017-04-18 22:27:36 +0800

    update code

--- pro-demo/src/main/java/net/xinqushi/service/ElasticSearchService.java
+++ pro-demo/src/main/java/net/xinqushi/service/ElasticSearchService.java
@@ -2,6 +2,10 @@ package net.xinqushi.service;
 
 import java.util.List;
 
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.index.query.QueryBuilder;
+import org.elasticsearch.search.SearchHit;
+
 import net.xinqushi.common.exceptions.CommonException;
 import net.xinqushi.orm.entity.City;
 
@@ -12,9 +16,82 @@ import net.xinqushi.orm.entity.City;
  * @Description:es服务
  */
 public interface ElasticSearchService {
-	
-	/** 将信息存入es*/
+
+	/** 将信息存入es */
 	public void putDoc(List<City> cities) throws CommonException;
+
+	/**
+	 * 单条索引创建,如果id為空，才用默認id
+	 * 
+	 * @param indexName
+	 * @param type
+	 * @param jsondata
+	 * @param id
+	 *            索引对象单独指定ID
+	 * @throws CommonException
+	 */
+	public void createSingleIndexResponse(String indexName, String type,
+
+			String jsondata, String id) throws CommonException;
+
+	/**
+	 * 删除指定索引
+	 * 
+	 * @param indexname
+	 * @param type
+	 * @param id
+	 * @throws CommonException
+	 */
+	public void delteIndexResponse(String indexname, String type, String id) throws CommonException;
+
+	/**
+	 * 按照查询结果，删除对应索引
+	 * 
+	 * @param searchHits
+	 * @param index
+	 * @param type
+	 * @throws CommonException
+	 */
+	public void delteIndexResponse(SearchHit[] searchHits, String index, String type) throws CommonException;
+	
+	/**
+	 * （指定索引）单条查询 根据索引，类型，及其id,查询，返回一个json字符串
+	 * @param indexname
+	 * @param type
+	 * @param id
+	 * @return
+	 * @throws CommonException
+	 */
+	public String getIndexById(String indexname, String type, 
+				String id) throws CommonException;
+	
+	/**
+	 * 分頁查詢，返回搜索結果SearchResponse
+	 * @param queryBuilder
+	 * @param indexname
+	 * @param type
+	 * @param sort
+	 * @param order
+	 * @param page
+	 * @param pageSize
+	 * @return
+	 * @throws CommonException
+	 */
+	public SearchResponse searcher(QueryBuilder queryBuilder, String indexname,
+				String type, String sort, String order, Integer page, Integer pageSize) throws CommonException;
+	
+	/**
+	 * 根据条件 统计个数
+	 */
+	public long countQuery(QueryBuilder queryBuilder, String indexname,
+				String type) throws CommonException;
+	
+	/**
+	 * 更新对应id 的索引数据
+	 */
+	public void updateIndex(String indexname, String type, String jsondata,
+				String id) throws CommonException;
+	
 	
 	
 }
--- pro-demo/src/main/java/net/xinqushi/service/impl/ElasticSearchServiceImpl.java
+++ pro-demo/src/main/java/net/xinqushi/service/impl/ElasticSearchServiceImpl.java
@@ -2,11 +2,22 @@ package net.xinqushi.service.impl;
 
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+
+import org.elasticsearch.action.delete.DeleteResponse;
+import org.elasticsearch.action.index.IndexRequestBuilder;
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.client.Client;
+import org.elasticsearch.index.query.QueryBuilder;
+import org.elasticsearch.search.SearchHit;
+import org.quartz.Job;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import groovyjarjarantlr.Lookahead;
 import net.xinqushi.common.exceptions.CommonException;
 import net.xinqushi.orm.entity.City;
 import net.xinqushi.orm.mapper.CityMapper;
@@ -17,8 +28,19 @@ public class ElasticSearchServiceImpl implements ElasticSearchService{
 	
 	private static Logger logger = LoggerFactory.getLogger(ElasticSearchServiceImpl.class);
 	
+	private Client client;
+	
 	@Autowired
 	private CityMapper cityMapper;
+	
+	@PostConstruct
+	public void init(){
+		try {
+			client = ElasticSearchUtil.getESClient(null, null);
+		} catch (Exception e) {
+			logger.info("fail to initialize es client");
+		}
+	}
 	
 	@Override
 	public void putDoc(List<City> cities) throws CommonException {
@@ -32,6 +54,76 @@ public class ElasticSearchServiceImpl implements ElasticSearchService{
 			logger.info("fail to store the city info to es", e);
 			throw new CommonException("城市信息存入es失败");
 		}
+	}
+
+	@Override
+	public void createSingleIndexResponse(String indexName, String type, String jsondata, String id)
+			throws CommonException {
+		try {
+			IndexRequestBuilder requestBuilder = this.client.prepareIndex(indexName, type).setRefresh(true);
+			//id判断:id为空时使用默认
+			if ((null != id) && (!id.equals(""))) {
+				requestBuilder.setId(id);
+			}
+			//创建
+			IndexResponse response = requestBuilder.setSource(jsondata).get();
+			long version = response.getVersion();
+			boolean created = response.isCreated();
+			if (created) {
+				logger.info("创建索引: {}:-<<: {} :-<<: {}", indexName, type, jsondata);
+			} else {
+				logger.info("索引存在,已重复至第{}版本: {}:-<<: {} :-<<: {} :-<<: {}", version, indexName, type, jsondata);
+			}
+		} catch (Exception e) {
+			logger.info("fail to create single index");
+			throw new CommonException("创建索引失败");
+		}
+	}
+
+	@Override
+	public void delteIndexResponse(String indexname, String type, String id) throws CommonException {
+		try {
+			if ((null != id) && (!id.isEmpty())) {
+				DeleteResponse response = client.prepareDelete(indexname, type, id).execute().actionGet();
+				if (response.isFound()) {
+					logger.info("删除索引: {} :-<<: {} :-<<: {}", indexname, type, id);
+				} else {
+					logger.info("不存在该索引: {} :-<<: {} :-<<: {}", indexname, type, id);
+				}
+			}
+		} catch (Exception e) {
+			logger.info("fail to delete single index");
+			throw new CommonException("删除索引失败");
+		}
+	}
+
+	@Override
+	public void delteIndexResponse(SearchHit[] searchHits, String index, String type) throws CommonException {
+		
+	}
+
+	@Override
+	public String getIndexById(String indexname, String type, String id) throws CommonException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public SearchResponse searcher(QueryBuilder queryBuilder, String indexname, String type, String sort, String order,
+			Integer page, Integer pageSize) throws CommonException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public long countQuery(QueryBuilder queryBuilder, String indexname, String type) throws CommonException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void updateIndex(String indexname, String type, String jsondata, String id) throws CommonException {
+		// TODO Auto-generated method stub
 		
 	}
 	
