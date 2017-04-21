f09f222ddfa63009865ff3fe1b45e873fa9674bb
yangli <350895216@qq.com>
2017-04-21 11:00:45 +0800

    新增springcloud配置服务器和注册服务器

---
+++ myconfig-service/src/main/java/com/utoo/config/ConfigServerApplication.java
@@ -0,0 +1,15 @@
+package com.utoo.config;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		new SpringApplicationBuilder(ConfigServerApplication.class).run(args);
+	}
+
+}
