b79fd8bb4184c5338fa358be692a71c9e0efba52
yangli <350895216@qq.com>
2017-08-04 16:19:04 +0800

    update code

--- mycommon-component/src/main/java/net/xinqushi/util/DateUtils.java
+++ mycommon-component/src/main/java/net/xinqushi/util/DateUtils.java
@@ -1,182 +1,516 @@
 package net.xinqushi.util;
 
+import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 
+import org.apache.commons.lang.time.DateFormatUtils;
 /**
- * 
- * 时间工具类。
- * 
+ * 时间工具类
+ * @author yangli
  */
-
 public class DateUtils {
 
-	/**
-	 * 
-	 * 时间 -> 字符串
-	 * 
-	 */
-	public static String format(Date date, String pattern) {
-
-		SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
-
-		return simpleDateFormat.format(date);
-
-	}
-
-	/**
-	 * 
-	 * 获取系统当前时间
-	 * 
-	 */
-	public static String nowTime() {
-
-		SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");// 设置日期格式
-
-		return df.format(new Date());// new Date()为获取当前系统时间
-
-	}
-	
-	public static String cast(Long timeSlot){
-		SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");// 设置日期格式
-
-		return df.format(timeSlot);// new Date()为获取当前系统时间
-	}
-	
-	/**
-	 * 
-	 * 字符串 -> 时间
-	 * 
-	 */
-	public static Date parse(String source) {
-
-		SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");// 设置日期格式
-
-		Date date = null;
-
-		try {
-
-			date = df.parse(source);
-
-		} catch (ParseException e) {
-
-			e.printStackTrace();
-
-		}
-
-		return date;
-
-	}
+	public static final String hhmmFormat = "HH:mm";
+	public static final String MMddFormat = "MM-dd";
+	public static final String yyyymmFormat = "yyyy-MM";
+	public static final String yyyyFormat = "yyyy";
+	public static final String yyyyChineseFormat = "yyyy年";
+	public static final String yyyyMMddFormat = "yyyy-MM-dd";
+	public static final String yyyyMMddFormat_NOSEPARATOR = "yyyyMMdd";
+	public static final String fullFormat = "yyyy-MM-dd HH:mm:ss";
+	public static final String MMddChineseFormat = "MM月dd日";
+	public static final String MMddHHmmChineseFormat = "MM月dd日 HH时mm分";
+	public static final String yyyyMMddChineseFormat = "yyyy年MM月dd日";
+	public static final String fullChineseFormat = "yyyy年MM月dd日 HH时mm分ss秒";
+	public static final String yyyyMMddHHmmssSSS = "yyyy-MM-dd HH:mm:ss:SSS";
+	public static final String[] WEEKS = { "星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六" };
+	public static final String NO_SEPARATOR = "yyyyMMddHHmmss";
+	public static final String NO_SEPARATOR_SSS = "yyyyMMddHHmmssSSS";
 
 	/**
-	 * 
-	 * 字符串 -> 时间(只有年月日)
-	 * 
-	 */
-	public static Date parseYMD(String source) {
-
-		SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");// 设置日期格式
-
-		Date date = null;
-
-		try {
-
-			date = df.parse(source);
-
-		} catch (ParseException e) {
-
-			e.printStackTrace();
-
-		}
-
-		return date;
-
-	}
-
-	/**
-	 * 
-	 * java.sql.Date -> java.util.Date
-	 * 
-	 */
-	public static Date cast(java.sql.Date date) {
-
-		return new Date(date.getTime());
-
-	}
-
-	/**
-	 * 
-	 * java.util.Date -> java.sql.Date
-	 * 
-	 * 
-	 * 
-	 * @param date
+	 * 返回当前日期 格式参照fullFormat
 	 * 
 	 * @return
-	 * 
 	 */
-	public static java.sql.Date cast(Date date) {
-
-		return new java.sql.Date(date.getTime());
-
+	public static String curDateStr() {
+		DateFormat df = new SimpleDateFormat(fullFormat);
+		return df.format(new Date());
 	}
 
 	/**
+	 * 得到指定时间的时间日期格式
 	 * 
-	 * 秒转为小时工具类
-	 * 
+	 * @param date
+	 *            指定的时间
+	 * @param format
+	 *            时间日期格式
+	 * @return
 	 */
-	public static String millisToString(int millis) {
-
-		String HH = null;
-
-		String mm = null;
-
-		String ss = null;
-
-		HH = Integer.toString(millis / 3600);
-
-		mm = Integer.toString(millis % 3600 / 60);
-
-		ss = Integer.toString(millis % 3600 % 60);
-
-		if ((millis % 3600 / 60) == 0) {
-
-			mm = "00";
-
-		}
-
-		if ((millis / 3600) == 0) {
-
-			HH = "00";
-
-		}
-
-		if ((millis % 3600 % 60) == 0) {
-
-			ss = "00";
-
-		}
-
-		if ((millis % 3600 / 60) < 10 && (millis % 3600 / 60) > 0) {
-
-			mm = null;
-
-			mm = "0" + millis % 3600 / 60;
-
-		}
-
-		if ((millis % 3600 % 60) < 10 && (millis % 3600 % 60) > 0) {
-
-			ss = null;
-
-			ss = "0" + millis % 3600 % 60;
-
-		}
-
-		return HH + ":" + mm + ":" + ss;
-
+	public static String getFormatDateTime(Date date, String format) {
+		DateFormat df = new SimpleDateFormat(format);
+		return df.format(date);
+	}
+	
+	/**
+	 * 得到指定时间的时间日期格式
+	 * 
+	 * @param date
+	 *            指定的时间
+	 * @param format
+	 *            时间日期格式
+	 * @return
+	 */
+	public static String getFormatDateTime(Long date, String format) {
+		DateFormat df = new SimpleDateFormat(format);
+		return df.format(date);
 	}
 
+	/**
+	 * 判断是否是润年
+	 * 
+	 * @param date
+	 *            指定的时间
+	 * @return true:是润年,false:不是润年
+	 */
+	public static boolean isLeapYear(Date date) {
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(date);
+		return isLeapYear(cal.get(Calendar.YEAR));
+	}
+
+	/**
+	 * 判断是否是润年
+	 * 
+	 * @param date
+	 *            指定的年
+	 * @return true:是润年,false:不是润年
+	 */
+	public static boolean isLeapYear(int year) {
+		GregorianCalendar calendar = new GregorianCalendar();
+		return calendar.isLeapYear(year);
+	}
+
+	/**
+	 * 判断指定的时间是否是今天
+	 * 
+	 * @param date
+	 *            指定的时间
+	 * @return true:是今天,false:非今天
+	 */
+	public static boolean isInToday(Date date) {
+		boolean flag = false;
+		Date now = new Date();
+		String fullFormat = getFormatDateTime(now, DateUtils.yyyyMMddFormat);
+		String beginString = fullFormat + " 00:00:00";
+		String endString = fullFormat + " 23:59:59";
+		DateFormat df = new SimpleDateFormat(DateUtils.fullFormat);
+		try {
+			Date beginTime = df.parse(beginString);
+			Date endTime = df.parse(endString);
+			flag = date.before(endTime) && date.after(beginTime);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		return flag;
+	}
+
+	/**
+	 * 判断两时间是否是同一天
+	 * 
+	 * @param from
+	 *            第一个时间点
+	 * @param to
+	 *            第二个时间点
+	 * @return true:是同一天,false:非同一天
+	 */
+	public static boolean isSameDay(Date from, Date to) {
+		boolean isSameDay = false;
+		DateFormat df = new SimpleDateFormat(DateUtils.yyyyMMddFormat);
+		String firstDate = df.format(from);
+		String secondDate = df.format(to);
+		isSameDay = firstDate.equals(secondDate);
+		return isSameDay;
+	}
+
+	/**
+	 * 求出指定的时间那天是星期几
+	 * 
+	 * @param date
+	 *            指定的时间
+	 * @return 星期X
+	 */
+	public static String getWeekString(Date date) {
+		return DateUtils.WEEKS[getWeek(date) - 1];
+	}
+
+	/**
+	 * 求出指定时间那天是星期几
+	 * 
+	 * @param date
+	 *            指定的时间
+	 * @return 1-7
+	 */
+	public static int getWeek(Date date) {
+		int week = 0;
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(date);
+		week = cal.get(Calendar.DAY_OF_WEEK);
+		return week;
+	}
+
+	/**
+	 * 取得指定时间离现在是多少时间以前，如：3秒前,2小时前等 注意：此计算方法不是精确的
+	 * 
+	 * @param date
+	 *            已有的指定时间
+	 * @return 时间段描述
+	 */
+	public static String getAgoTimeString(Date date) {
+		Date now = new Date();
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(date);
+		Date agoTime = cal.getTime();
+		long mtime = now.getTime() - agoTime.getTime();
+		String str = "";
+		long stime = mtime / 1000;
+		long minute = 60;
+		long hour = 60 * 60;
+		long day = 24 * 60 * 60;
+		long weeks = 7 * 24 * 60 * 60;
+		long months = 100 * 24 * 60 * 60;
+		if (stime < minute) {
+			long time_value = stime;
+			if (time_value <= 0) {
+				time_value = 1;
+			}
+			str = time_value + "秒前";
+		} else if (stime >= minute && stime < hour) {
+			long time_value = stime / minute;
+			if (time_value <= 0) {
+				time_value = 1;
+			}
+			str = time_value + "分前";
+		} else if (stime >= hour && stime < day) {
+			long time_value = stime / hour;
+			if (time_value <= 0) {
+				time_value = 1;
+			}
+			str = time_value + "小时前";
+		} else if (stime >= day && stime < weeks) {
+			long time_value = stime / day;
+			if (time_value <= 0) {
+				time_value = 1;
+			}
+			str = time_value + "天前";
+		} else if (stime >= weeks && stime < months) {
+			DateFormat df = new SimpleDateFormat(DateUtils.MMddFormat);
+			str = df.format(date);
+		} else {
+			DateFormat df = new SimpleDateFormat(DateUtils.yyyyMMddFormat);
+			str = df.format(date);
+		}
+		return str;
+	}
+
+	/**
+	 * 判断指定时间是否是周末
+	 * 
+	 * @param date
+	 *            指定的时间
+	 * @return true:是周末,false:非周末
+	 */
+	public static boolean isWeeks(Date date) {
+		boolean isWeek = false;
+		isWeek = (getWeek(date) - 1 == 0 || getWeek(date) - 1 == 6);
+		return isWeek;
+	}
+
+	/**
+	 * 得到今天的最开始时间
+	 * 
+	 * @return 今天的最开始时间
+	 */
+	public static Date getTodayBeginTime() {
+		String beginString = DateUtils.yyyyMMddFormat + " 00:00:00";
+		DateFormat df = new SimpleDateFormat(DateUtils.fullFormat);
+		Date beginTime = new Date();
+		try {
+			beginTime = df.parse(beginString);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		return beginTime;
+	}
+
+	/**
+	 * 得到今天的最开始时间
+	 * 
+	 * @return 今天的最开始时间
+	 */
+	public static Date getDateBeginTime(Date date) {
+		String dateString = DateFormatUtils.format(date, "yyyy-MM-dd 00:00:00");
+		Date beginTime = null;
+		try {
+			beginTime = org.apache.commons.lang.time.DateUtils.parseDate(dateString,
+					new String[] { "yyyy-MM-dd hh:mm:ss" });
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		return beginTime;
+	}
+
+	/**
+	 * 得到今天的最后结束时间
+	 * 
+	 * @return 今天的最后时间
+	 */
+	public static Date getTodayEndTime() {
+		String endString = DateUtils.yyyyMMddFormat + " 23:59:59";
+		DateFormat df = new SimpleDateFormat(DateUtils.fullFormat);
+		Date endTime = new Date();
+		try {
+			endTime = df.parse(endString);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		return endTime;
+	}
+
+	/**
+	 * 得到今天的最后结束时间
+	 * 
+	 * @return 今天的最后时间
+	 */
+	public static Date getDateEndTime(Date date) {
+		String dateString = DateFormatUtils.format(date, "yyyy-MM-dd 23:59:59");
+		Date endTime = null;
+		try {
+			endTime = org.apache.commons.lang.time.DateUtils.parseDate(dateString,
+					new String[] { "yyyy-MM-dd hh:mm:ss" });
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		return endTime;
+	}
+
+	/**
+	 * 取得本周的开始时间
+	 * 
+	 * @return 本周的开始时间
+	 */
+	public static Date getThisWeekBeginTime() {
+		Date beginTime = null;
+		Calendar cal = Calendar.getInstance();
+		int week = getWeek(cal.getTime());
+		week = week - 1;
+		int days = 0;
+		if (week == 0) {
+			days = 6;
+		} else {
+			days = week - 1;
+		}
+		cal.add(Calendar.DAY_OF_MONTH, -days);
+		beginTime = cal.getTime();
+		return beginTime;
+	}
+
+	/**
+	 * 指定时间获取周开始时间
+	 * 
+	 * @return
+	 */
+	public static Date getWeekBeginTime(Date date) {
+		Date beginTime = null;
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(date);
+		int week = getWeek(cal.getTime());
+		week = week - 1;
+		int days = 0;
+		if (week == 0) {
+			days = 6;
+		} else {
+			days = week - 1;
+		}
+		cal.add(Calendar.DAY_OF_MONTH, -days);
+		beginTime = cal.getTime();
+		return beginTime;
+	}
+
+	/**
+	 * 取得本周的开始日期
+	 * 
+	 * @param format
+	 *            时间的格式
+	 * @return 指定格式的本周最开始时间
+	 */
+	public static String getThisWeekBeginTimeString(String format) {
+		DateFormat df = new SimpleDateFormat(format);
+		return df.format(getThisWeekBeginTime());
+	}
+
+	/**
+	 * 取得本周的结束时间
+	 * 
+	 * @return 本周的结束时间
+	 */
+	public static Date getThisWeekEndTime() {
+		Date endTime = null;
+		Calendar cal = Calendar.getInstance();
+		int week = getWeek(cal.getTime());
+		week = week - 1;
+		int days = 0;
+		if (week != 0) {
+			days = 7 - week;
+		}
+		cal.add(Calendar.DAY_OF_MONTH, days);
+		endTime = cal.getTime();
+		return endTime;
+	}
+
+	/**
+	 * 获取指定时间周的结束时间
+	 * 
+	 * @return
+	 */
+	public static Date getWeekEndTime(Date date) {
+		Date endTime = null;
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(date);
+		int week = getWeek(cal.getTime());
+		week = week - 1;
+		int days = 0;
+		if (week != 0) {
+			days = 7 - week;
+		}
+		cal.add(Calendar.DAY_OF_MONTH, days);
+		endTime = cal.getTime();
+		return endTime;
+	}
+
+	/**
+	 * 取得本周的结束日期
+	 * 
+	 * @param format
+	 *            时间的格式
+	 * @return 指定格式的本周结束时间
+	 */
+	public static String getThisWeekEndTimeString(String format) {
+		DateFormat df = new SimpleDateFormat(format);
+		return df.format(getThisWeekEndTime());
+	}
+
+	/**
+	 * 取得两时间相差的天数
+	 * 
+	 * @param from
+	 *            第一个时间
+	 * @param to
+	 *            第二个时间
+	 * @return 相差的天数
+	 */
+	public static long getBetweenDays(Date from, Date to) {
+		long days = 0;
+		long dayTime = 24 * 60 * 60 * 1000;
+		long fromTime = from.getTime();
+		long toTime = to.getTime();
+		long times = Math.abs(fromTime - toTime);
+		days = times / dayTime;
+		return days;
+	}
+
+	/**
+	 * 取得两时间相差的小时数
+	 * 
+	 * @param from
+	 *            第一个时间
+	 * @param to
+	 *            第二个时间
+	 * @return 相差的小时数
+	 */
+	public static long getBetweenHours(Date from, Date to) {
+		long hours = 0;
+		long hourTime = 60 * 60 * 1000;
+		long fromTime = from.getTime();
+		long toTime = to.getTime();
+		long times = Math.abs(fromTime - toTime);
+		hours = times / hourTime;
+		return hours;
+	}
+
+	/**
+	 * 取得在指定时间上加减days天后的时间
+	 * 
+	 * @param date
+	 *            指定的时间
+	 * @param days
+	 *            天数,正为加，负为减
+	 * @return 在指定时间上加减days天后的时间
+	 */
+	public static Date addDays(Date date, int days) {
+		Date time = null;
+		Calendar cal = Calendar.getInstance();
+		cal.add(Calendar.DAY_OF_MONTH, days);
+		time = cal.getTime();
+		return time;
+	}
+
+	/**
+	 * 取得在指定时间上加减months月后的时间
+	 * 
+	 * @param date
+	 *            指定时间
+	 * @param months
+	 *            月数，正为加，负为减
+	 * @return 在指定时间上加减months月后的时间
+	 */
+	public static Date addMonths(Date date, int months) {
+		Date time = null;
+		Calendar cal = Calendar.getInstance();
+		cal.add(Calendar.MONTH, months);
+		time = cal.getTime();
+		return time;
+	}
+
+	/**
+	 * 取得在指定时间上加减years年后的时间
+	 * 
+	 * @param date
+	 *            指定时间
+	 * @param years
+	 *            年数，正为加，负为减
+	 * @return 在指定时间上加减years年后的时间
+	 */
+	public static Date addYears(Date date, int years) {
+		Date time = null;
+		Calendar cal = Calendar.getInstance();
+		cal.add(Calendar.YEAR, years);
+		time = cal.getTime();
+		return time;
+	}
+
+	public static long toMillis(String dateStr) throws ParseException {
+		SimpleDateFormat format = new SimpleDateFormat(yyyyMMddHHmmssSSS);
+		Date date = format.parse(dateStr);
+		return date.getTime();
+	}
+
+	public static long toMillis(String dateStr, String formatRegex) throws ParseException {
+		SimpleDateFormat format = new SimpleDateFormat(formatRegex);
+		Date date = format.parse(dateStr);
+		return date.getTime();
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println(getWeekBeginTime(org.apache.commons.lang.time.DateUtils.addWeeks(new Date(), -1)));
+
+		System.out.println(getWeekEndTime(org.apache.commons.lang.time.DateUtils.addWeeks(new Date(), -1)));
+
+	}
 }
--- pro-demo/src/test/java/net/xinqushi/DemoTest.java
+++ pro-demo/src/test/java/net/xinqushi/DemoTest.java
@@ -1,25 +1,7 @@
 package net.xinqushi;
 
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.List;
-import java.util.Map;
-import java.util.NavigableMap;
-import java.util.Set;
-import java.util.TreeMap;
-import java.util.Map.Entry;
-
-import com.alibaba.fastjson.JSON;
-
-import net.xinqushi.common.constants.CacheConstants;
-import net.xinqushi.common.enums.CommonStatus;
 import net.xinqushi.util.DateUtils;
 import net.xinqushi.util.JedisPoolUtil;
-import net.xinqushi.vo.LineSetting;
-import net.xinqushi.vo.LineTimePriceSetting;
-import net.xinqushi.vo.PriceRule;
-import net.xinqushi.vo.TimeSlot;
-import net.xinqushi.vo.TimeSlotCast;
 import redis.clients.jedis.Jedis;
 
 public class DemoTest {
@@ -28,9 +10,25 @@ public class DemoTest {
 //		getAvailableTimeSlots(33, "SHARE", 4);
 		
 		Jedis jedis = JedisPoolUtil.getJedis();
-		jedis.set("test", "测试");
-		System.out.println(jedis.get("test"));
+//		jedis.set("sequence:consumption", "1");
+//		System.out.println(jedis.get("sequence:consumption"));
 		
+		String cast = DateUtils.getFormatDateTime(System.currentTimeMillis(), DateUtils.yyyyMMddFormat_NOSEPARATOR);
+//		System.out.println(cast);
+		
+		
+		StringBuilder orderNumber = new StringBuilder("TD");
+		if (null == jedis.get("sequence:consumption:" + cast)) {
+			jedis.set("sequence:consumption:" + cast, 1 + "");
+			orderNumber.append(cast).append(String.format("%04d", 1));
+		} else {
+			Long incr = jedis.incr("sequence:consumption:" + cast);
+			orderNumber.append(cast).append(String.format("%04d", incr));
+		}
+		System.out.println("orderNumber = " + orderNumber);
+		
+//		jedis.set("test", "测试");
+//		System.out.println(jedis.get("test"));
 		
 //		System.out.println(CommonStatus.toString(CommonStatus.VALID));
 //		System.out.println(CommonStatus.VALID.toString());
@@ -38,158 +36,4 @@ public class DemoTest {
 
 	}
 
-	/**
-	 * 获取可用预约时间列表
-	 * 
-	 * @param lineId
-	 *            线路id
-	 * @param tripMode
-	 *            乘车方式：包、拼车
-	 * @param seats
-	 *            座位数
-	 */
-	public static void getAvailableTimeSlots(int lineId, String tripMode, int seats) {
-
-		// 从缓存获取线路相关设置
-		String str = JedisPoolUtil.getJedis().hget(CacheConstants.LINE_SETTINGS_KEY, lineId + "");
-		System.out.println("str = " + str);
-		LineSetting lineSetting = JSON.parseObject(str, LineSetting.class);
-
-		// 从线路设置中取出拼车时间设置
-		Long startTime = System.currentTimeMillis();
-		Calendar cal2 = Calendar.getInstance();
-		cal2.setTimeInMillis(startTime);
-		int hour = cal2.get(Calendar.HOUR_OF_DAY);
-		int min = cal2.get(Calendar.MINUTE);
-		int idx = hour * 2;
-		if (min >= 30) {
-			idx += 1;
-		}
-		
-		LineTimePriceSetting setting = null;
-		List<LineTimePriceSetting> timePriceSettings = null;
-		if (tripMode.equals("SHARE")) {
-			timePriceSettings = lineSetting.getShareTimeSettings();
-		} else {
-			timePriceSettings = lineSetting.getChartorTimeSettings();
-		}
-		for (LineTimePriceSetting timePriceSetting : timePriceSettings) {
-			if (timePriceSetting.getBeginTime() <= idx && idx <= timePriceSetting.getEndTime()) {
-				setting = timePriceSetting;
-				break;
-			}
-		}
-		System.out.println(setting);
-		// 拼接
-		TreeMap<Long, TimeSlot> slots = new TreeMap<Long, TimeSlot>();
-		for (LineTimePriceSetting pricSetting : timePriceSettings) {
-			// 获取最大座位数
-			int maxSeat = 0;
-			Map<Integer, List<PriceRule>> carModelPriceRules = pricSetting.getCarModelPriceRules();
-			Set<Integer> keySet = carModelPriceRules.keySet();
-			for (Integer integer : keySet) {
-				if (maxSeat < integer) {
-					maxSeat = integer;
-				}
-			}
-
-			int begin = pricSetting.getBeginTime();
-			int end = pricSetting.getEndTime();
-			int interval = pricSetting.getIntervalMin();
-			int maxReservDay = pricSetting.getMaxReservDay();
-
-			int beginHour = begin / 2;
-			int beginMin = begin % 2 == 0 ? 0 : 30;
-
-			int endHour = end / 2;
-			int endMin = end % 2 == 0 ? 0 : 30;
-
-			long step = interval * 60 * 1000;
-			int count = 0;
-			while (count < maxReservDay) {
-				Calendar cal = Calendar.getInstance();
-				cal.add(Calendar.DATE, count);
-				cal.set(Calendar.HOUR_OF_DAY, beginHour);
-				cal.set(Calendar.MINUTE, beginMin);
-				cal.set(Calendar.SECOND, 0);
-				cal.set(Calendar.MILLISECOND, 0);
-
-				Calendar calEnd = Calendar.getInstance();
-				calEnd.add(Calendar.DATE, count);
-				calEnd.set(Calendar.HOUR_OF_DAY, endHour);
-				calEnd.set(Calendar.MINUTE, endMin);
-
-				long slotBegin = cal.getTimeInMillis();
-				long slotEnd = slotBegin + step;
-				long rangeEnd = calEnd.getTimeInMillis();
-
-				while (slotEnd <= rangeEnd) {
-					if (!slots.containsKey(slotBegin)) {
-						TimeSlot slot = new TimeSlot(slotBegin, slotEnd);
-						slot.setMaxSeats(maxSeat);
-						slots.put(slotBegin, slot);
-					}
-					slotBegin = slotEnd;
-					slotEnd += step;
-				}
-				count++;
-			}
-		}
-		System.out.println("该线路的合法预约时段：" + JSON.toJSONString(slots));
-
-		// 按当前时间过滤
-		Long start = startTime;
-		if (setting != null && setting.getMinReservMin() > 0) {
-			start += setting.getMinReservMin() * 60000;
-		}
-		NavigableMap<Long, TimeSlot> submap = slots.tailMap(start, true);
-		int ahead = null == setting ? 7 : setting.getMaxReservDay();
-		Calendar cal = Calendar.getInstance();
-		cal.add(Calendar.DAY_OF_MONTH, ahead);
-		long end = cal.getTimeInMillis();
-		submap = submap.headMap(end, false);
-		Entry<Long, TimeSlot> entry = slots.floorEntry(submap.firstKey() - 1);
-		if (entry != null) {
-			TimeSlot slot = entry.getValue();
-			if (slot != null) {
-				if (slot.getSlotBegin() <= start && start <= slot.getSlotEnd()) {
-					TreeMap<Long, TimeSlot> result = new TreeMap<Long, TimeSlot>();
-					result.put(entry.getKey(), slot);
-					result.putAll(submap);
-
-					submap = result;
-				}
-			}
-		}
-		System.out.println("按当前时间过滤后：" + JSON.toJSONString(submap));
-
-		// 按座位数过滤
-		List<Long> removeList = new ArrayList<Long>();
-
-		for (TimeSlot slot : submap.values()) {
-			if (slot.getMaxSeats() < seats) {
-				removeList.add(slot.getSlotBegin());
-			}
-		}
-		if (removeList.size() > 0) {
-			for (Long time : removeList) {
-				submap.remove(time);
-			}
-		}
-		System.out.println("按当前时间+座位数过滤后：" + JSON.toJSONString(submap));
-
-		// long timeSlot = System.currentTimeMillis();
-		// System.out.println(DateUtils.cast(timeSlot));
-
-		// timeSlot -> cast
-		TreeMap<String, TimeSlotCast> castSlots = new TreeMap<String, TimeSlotCast>();
-		for (TimeSlot timeSlot : submap.values()) {
-			TimeSlotCast tsc = new TimeSlotCast();
-			tsc.setMaxSeats(timeSlot.getMaxSeats());
-			tsc.setSlotBegin(DateUtils.cast(timeSlot.getSlotBegin()));
-			tsc.setSlotEnd(DateUtils.cast(timeSlot.getSlotEnd()));
-			castSlots.put(tsc.getSlotBegin(), tsc);
-		}
-		System.out.println("转换后：" + JSON.toJSONString(castSlots));
-	}
 }
